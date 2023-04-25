c77ad1e4cde48275123443c2e79d837f7db6de51
unknown <mkgoma3@gmail.com>
2023-04-25 10:28:50 +0900

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './Header.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+// props として受け取る型の定義（`Props`部分の名前はどんな名前でも可）
+type Props = {
+  title: string
+}
+
+const Header: React.FC<Props> = (props) => {
+  return (
+    <header className={styles.header}>
+      <Link href="/" >
+        TOP
+      </Link>
+      <Link href="/about" >
+        ABOUT
+      </Link>
+      <h2>{props.title}</h2>
+    </header>
+  )
+}
+
+export { Header }
