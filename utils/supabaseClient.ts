289648d02c4cdb163b8b896f8575737f0645a712
mkmkmkn <mkgoma3@gmail.com>
2023-06-11 18:04:55 +0900

    型エラー修正

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-export type Database = {};
+export type Database = {
+    id: string,
+    task: string,
+    created_at: string,
+    order: number
+};
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
