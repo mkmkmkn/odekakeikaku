c96bb791daf90a050497be52168f2bf2700cbb3f
mkmkmkn <mkgoma3@gmail.com>
2023-06-13 23:58:21 +0900

    型エラー時ビルド

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // 型エラー時もビルド
+    ignoreBuildErrors: true
+  }
 }
 
 module.exports = nextConfig
