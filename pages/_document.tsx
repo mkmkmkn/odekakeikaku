3266c7995bec8ab356bf3e5411ce88b5e7415816
mkmkmkn <mkgoma3@gmail.com>
2023-06-22 18:50:40 +0900

    Material Symbols Outlinedフォント　読み込み終わるまで非表示

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,9 @@ export default function Document() {
       <link rel="apple-touch-icon" href="/icon.png"></link>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;500;700&family=Kiwi+Maru:wght@300;400;500&display=swap" rel="stylesheet" />
       {/* loads the entire Variable Font */}
-      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0&display=auto" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;500;700&family=Kiwi+Maru:wght@300;400;500&display=swap" rel="stylesheet" />
     </Head>
       <body>
         <Main />
